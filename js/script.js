2113740b84388e836cd0b76f0ca898bfc73646bc
samokhindm <samokhindm@gmail.com>
2021-11-26 18:41:05 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+"use strict";
+const answers = [];
+ answers[0] = prompt("Какое ваше имя?","");
+ answers[1] = prompt("Какая у вас вамилия?","");
+ answers[2] = prompt("Сколько вам лет?","");
+ 
+ document.write(answers);
